4b4d415d6aeeaff63056a26a7ddeb6156be20ced
kahironimashte <lanferia@web.de>
2020-05-02 21:40:02 +0200

    just conf.org

--- .emacs.d/elpa/git-commit-20200207.1819/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "88d09c30f4517df6dc1a2d422aa07a473c2235ac" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- .emacs.d/elpa/org-caldav-20191024.724/org-caldav-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-caldav" "20191024.724" "Sync org files with external calendar through CalDAV" '((org "7")) :commit "f530b94b6f8d8d1f8a207e48986da75227bd78a0" :keywords '("calendar" "caldav") :authors '(("David Engster" . "deng@randomsample.de")) :maintainer '("David Engster" . "deng@randomsample.de"))
