78fb86fe40ba4685fad6573b345389d848182ca7
kahrionimashte <lanferia@web.de>
2021-08-01 00:33:17 +0200

    category filter

--- dotfilesetup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init --bare $HOME/.myconfig
-alias config='/usr/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-echo "alias config='/usr/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'" >> $HOME/.baliases
