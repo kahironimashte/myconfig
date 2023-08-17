1c770e2c0c52e2ac68ab575c9e842efa2e3da14d
kahrionimashte <lanferia@web.de>
2023-08-17 12:12:08 +0200

    regexp-settings

--- .emacs.d/linux-custom.el
+++ .emacs.d/linux-custom.el
@@ -36,7 +36,9 @@
  '(org-safe-remote-resources
    '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
-   '(use-package corfu org-transclusion vundo consult orderless marginalia vertico org-appear org-sticky-header rainbow-delimiters magit org-remark olivetti unfill ess org-clock-convenience which-key imenu-anywhere anki-editor minions yaml-mode steam yasnippet ztree smex ryo-modal org-recur org-bullets nov modus-themes json-mode js2-mode ido-yes-or-no el-patch)))
+   '(use-package corfu org-transclusion vundo consult orderless marginalia vertico org-appear org-sticky-header rainbow-delimiters magit org-remark olivetti unfill ess org-clock-convenience which-key imenu-anywhere anki-editor minions yaml-mode steam yasnippet ztree smex ryo-modal org-recur org-bullets nov modus-themes json-mode js2-mode ido-yes-or-no el-patch))
+ '(reb-re-syntax 'string)
+ '(warning-suppress-types '((org-element-cache))))
 ;; FONTS
 ;; -----
 (custom-set-faces
